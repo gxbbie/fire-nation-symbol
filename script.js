0421d33668114afeaa79993f75213bf2ac7183fa
Gabrielle Listana <gabbielistana@gmail.com>
2020-07-24 01:18:45 +0000

    bye bye fire nation hello water tribe

--- script.js
+++ script.js
@@ -4,16 +4,23 @@ function setup() {
 
 function draw() {
   background(220);
-  drawFireNation();
+  // drawFireNation();
+
+   noFill();
+  stroke(5);
+  circle(width/2, height/2, 100);
+  circle(225, 225, 25);
+  circle(215, 250, 25);
+  circle(250, 220, 25); 
 }
 
-function drawFireNation() {
-  noFill();
-  strokeWeight(5);
-  stroke(color(161, 0, 0));
+// function drawFireNation() {
+//   noFill();
+//   strokeWeight(5);
+//   stroke(color(161, 0, 0));
 
-  triangle(200, 275, 220, 275, 210, 220);
-  triangle(280, 275, 300, 275, 290, 210);
-  triangle(230, 220, 100, 220, 200, 300)
-  ellipse(250, 250, 60, 60);
-}
+//   triangle(200, 275, 220, 275, 210, 220);
+//   triangle(280, 275, 300, 275, 290, 210);
+//   triangle(230, 220, 100, 220, 200, 300)
+//   ellipse(250, 250, 60, 60);
+// }
