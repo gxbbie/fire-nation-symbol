6e9f869ffdf95ecc89fc07bd97768bbd11662329
Gabrielle Listana <gabbielistana@gmail.com>
2020-07-24 01:46:14 +0000

    Added Water Tribe symbol and finished Fire Nation symbol.

--- script.js
+++ script.js
@@ -4,23 +4,25 @@ function setup() {
 
 function draw() {
   background(220);
-  // drawFireNation();
+  drawFireNation();
 
    noFill();
-  stroke(5);
-  circle(width/2, height/2, 100);
-  circle(225, 225, 25);
-  circle(215, 250, 25);
-  circle(250, 220, 25); 
+  strokeWeight(2);
+  stroke('blue');
+  circle(width/5, height/5, 100);
+  circle(70, 113, 25);
+  circle(77, 86, 25);
+  circle(95, 65, 25); 
 }
 
-// function drawFireNation() {
-//   noFill();
-//   strokeWeight(5);
-//   stroke(color(161, 0, 0));
+function drawFireNation() {
+  noFill();
+  strokeWeight(5);
+  stroke(color(161, 0, 0));
 
-//   triangle(200, 275, 220, 275, 210, 220);
-//   triangle(280, 275, 300, 275, 290, 210);
-//   triangle(230, 220, 100, 220, 200, 300)
-//   ellipse(250, 250, 60, 60);
-// }
+  triangle(200, 275, 220, 275, 210, 220);
+  triangle(280, 275, 300, 275, 290, 210);
+  triangle(220, 220, 280, 220, 250, 160)
+  arc(250, 260, 86, 80, 0, PI, OPEN);
+  ellipse(250, 250, 60, 60);
+}
